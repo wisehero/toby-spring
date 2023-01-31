84e60ef60b557114ad4512bd544e05c56aa68620
wisehero10 <godjw96@gmail.com>
2023-01-31 23:55:55 +0900

    # TEST - 테스트 코드를 이용한 테스트

---
+++ src/test/java/tobyspring/helloboot/HelloApiTest.java
@@ -0,0 +1,26 @@
+package tobyspring.helloboot;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+
+public class HelloApiTest {
+
+	@Test
+	void helloApi() {
+		// http localhost:8080/hello?name=Spring
+		TestRestTemplate rest = new TestRestTemplate();
+
+		ResponseEntity<String> response = rest.getForEntity("http://localhost:8080/hello?name={name}", String.class,
+				"SpringBoot");
+
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+		assertThat(response.getHeaders().getFirst(HttpHeaders.CONTENT_TYPE)).startsWith(MediaType.TEXT_PLAIN_VALUE);
+		assertThat(response.getBody()).isEqualTo("Hello SpringBoot");
+	}
+}
