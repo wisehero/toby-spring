53d4e5db187bf1d8d951c45cf79e61e3affb6ecc
wisehero10 <godjw96@gmail.com>
2023-01-31 18:31:15 +0900

    # ADD - @ComponentScan

--- src/main/java/tobyspring/helloboot/HelloController.java
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -4,9 +4,10 @@ import java.util.Objects;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping
+@RestController
 public class HelloController {
 
 	private final HelloService helloService;
@@ -16,7 +17,6 @@ public class HelloController {
 	}
 
 	@GetMapping("/hello")
-	@ResponseBody
 	public String hello(String name) {
 		return helloService.sayHello(Objects.requireNonNull(name));
 	}
--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -3,20 +3,23 @@ package tobyspring.helloboot;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
+@Configuration // 구성 정보를 가지고 있는 클래스라는 것을 알려준다.
+@ComponentScan
 public class HellobootApplication {
 
 	public static void main(String[] args) {
 
 		// 스프링 컨테이너 띄우기
-		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext() {
+		AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
 			// 스프링 컨테이너로 통합하기
 			@Override
 			protected void onRefresh() {
 				super.onRefresh();
-
 
 				ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
 				WebServer webServer = serverFactory.getWebServer(servletContext ->
@@ -26,11 +29,10 @@ public class HellobootApplication {
 
 			}
 		};
-		applicationContext.registerBean(HelloController.class);
-		applicationContext.registerBean(SimpleHelloService.class);
+		applicationContext.register(HellobootApplication.class);
 		applicationContext.refresh();
 
-		// // 톰캣을 쉽게 띄우기 위한 코드, 복잡한 과정 및 설정 없이 톰캣 서블릿 띄우기 가능
+		// 톰캣을 쉽게 띄우기 위한 코드, 복잡한 과정 및 설정 없이 톰캣 서블릿 띄우기 가능
 		// ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
 		// WebServer webServer = serverFactory.getWebServer(servletContext ->
 		// 		servletContext.addServlet("dispatcherServlet",
