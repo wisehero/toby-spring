79b374e333ed7d41f8217d977399ff1f251e5848
wisehero10 <godjw96@gmail.com>
2023-02-01 15:00:30 +0900

    # ADD - 합성 애노테이션 적용

---
+++ src/main/java/tobyspring/helloboot/MySpringBootAnnotation.java
@@ -0,0 +1,16 @@
+package tobyspring.helloboot;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Retention(RetentionPolicy.RUNTIME) // 왜 RUNTIME으로 줄까? -> 런타임까지 애노테이션이 유지되도록 하기 위함이다.
+@Target(ElementType.TYPE) // Type = [ "class", "method", "enum" ]
+@Configuration
+@ComponentScan
+public @interface MySpringBootAnnotation {
+}
--- src/test/java/tobyspring/helloboot/HelloServiceTest.java
+++ src/test/java/tobyspring/helloboot/HelloServiceTest.java
@@ -1,10 +1,22 @@
 package tobyspring.helloboot;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@Test
+@interface UnitTest {
+
+}
+
 public class HelloServiceTest {
-	@Test
+	@UnitTest
 	void simpleHelloService() {
 		SimpleHelloService helloService = new SimpleHelloService();
 		String ret = helloService.sayHello("Test");
