cbac3379e13e6f7f379087c3c97bbbba1173bde8
wisehero10 <godjw96@gmail.com>
2023-02-01 16:06:13 +0900

    # ADD - 자동 구성 정보 파일 분리

--- src/main/java/tobyspring/config/autoconfig/MyAutoConfigImportSelector.java
+++ src/main/java/tobyspring/config/autoconfig/MyAutoConfigImportSelector.java
@@ -1,14 +1,28 @@
 package tobyspring.config.autoconfig;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.boot.context.annotation.ImportCandidates;
 import org.springframework.context.annotation.DeferredImportSelector;
 import org.springframework.core.type.AnnotationMetadata;
 
+import tobyspring.config.MyAutoConfiguration;
+
 public class MyAutoConfigImportSelector implements DeferredImportSelector {
+
+	private final ClassLoader classLoader;
+
+	public MyAutoConfigImportSelector(ClassLoader classLoader) {
+		this.classLoader = classLoader;
+	}
+
 	@Override
 	public String[] selectImports(AnnotationMetadata importingClassMetadata) {
-		return new String[] {
-				"tobyspring.config.autoconfig.DispatcherServletConfig",
-				"tobyspring.config.autoconfig.TomcatWebServerConfig"
-		};
+		List<String> autoConfigs = new ArrayList<>();
+
+		ImportCandidates.load(MyAutoConfiguration.class, classLoader).forEach(autoConfigs::add);
+
+		return autoConfigs.toArray(new String[0]);
 	}
 }
